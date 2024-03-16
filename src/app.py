e9bb9e65c65e09d97039596c358e2135e31e7a47
Vaidik Lotan <codevaidik@gmail.com>
2024-03-16 15:31:57 +0530

    added proper files and routes

--- src/app.py
+++ src/app.py
@@ -37,28 +37,23 @@ def request_loader(request):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='email' id='email' placeholder='email'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
+        return flask.render_template('login.html')
 
     email = flask.request.form['email']
     if email in users and flask.request.form['password'] == users[email]['password']:
         user = User()
         user.id = email
         flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
+        return flask.redirect(flask.url_for('dashboard'))
 
-    return 'Bad login'
+    flask.flash('Wrong username or password!')
+    return flask.redirect(flask.url_for('login'))
 
 
-@app.route('/protected')
+@app.route('/dashboard')
 @flask_login.login_required
-def protected():
-    return 'Logged in as: ' + flask_login.current_user.id
+def dashboard():
+    return flask.render_template('dashboard.html')
 
 @app.route('/')
 @app.route('/home')
